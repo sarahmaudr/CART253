158d9f14a9c95044766d766d80a546912321eb8f
Sarah <smroy8@ulaval.ca>
2026-09-10 11:50:59 -0400

    commenting

--- topics/function-calls/js/script.js
+++ topics/function-calls/js/script.js
@@ -1,25 +1,35 @@
 /**
- * Title of Project : 
+ * Title of Project : The Rectangle
  * Author Name : Sarah-Maude Roy
  * 
  * DESCRIPTION : 
+ * An exploration of the existential angst of a novelist confronting
+ * the blank page while under huge pressure from their publisher. The
+ * program is non-interactive intentionally in order to simulate the
+ * writer's inability to get started on the project.
  * 
+ * Uses:
+ * p5.js
+ * https://p5js.org/
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Creates a 640x480 canvas for our masterpiece
+ */
 function setup() {
+    // Create the canvas at a standard resolution
     createCanvas(640, 480);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draws a blank piece of paper on a pink background
+ */
 function draw() {
+    // A pink background
     background(255, 100, 100);
+    // The blank piece of paper
     rect(200, 80, 240, 320);
 }
