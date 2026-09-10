74fbeba85a8e5ead46e33132e80d677d9a2bc766
Sarah <smroy8@ulaval.ca>
2026-09-10 11:32:24 -0400

    creating a rectangle

--- topics/function-calls/js/script.js
+++ topics/function-calls/js/script.js
@@ -1,9 +1,9 @@
 /**
- * Title of Project
- * Author Name
+ * Title of Project : 
+ * Author Name : Sarah-Maude Roy
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * DESCRIPTION : 
+ * 
  */
 
 "use strict";
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640, 480);
 }
 
 
@@ -20,5 +20,6 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+    background(255, 100, 100);
+    rect(200, 80, 240, 320);
 }
