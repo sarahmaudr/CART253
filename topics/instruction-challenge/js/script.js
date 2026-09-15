79dc66a1ca5edcc098f132da5510b7e7289c7231
Sarah <smroy8@ulaval.ca>
2026-09-15 15:27:47 -0400

    drawing clouds

--- topics/instruction-challenge/js/script.js
+++ topics/instruction-challenge/js/script.js
@@ -12,16 +12,55 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * creating a canvas
 */
 function setup() {
+    createCanvas(1000,500);
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * creating the landscape
 */
 function draw() {
+    drawSky();
+    drawSun();
+    drawClouds();
+}
 
+function drawSky(){
+// color of the sky
+    background("lightblue");
+}
+
+function drawSun(){
+// drawing a sun
+    push();
+    noStroke();
+    fill("yellow");
+    circle(100, 100, 150);
+    pop();
+}
+
+function drawClouds(){
+// drawing clouds
+    push();
+    noStroke();
+    fill(255);
+    ellipse(300, 100, 75, 75);
+    ellipse(260, 115, 60, 60);
+    ellipse(300, 125, 75, 75);
+    ellipse(340, 115, 60, 60);
+
+    ellipse(750, 100, 75, 75);
+    ellipse(710, 115, 60, 60);
+    ellipse(750, 125, 75, 75);
+    ellipse(790, 115, 60, 60);
+
+    ellipse(525, 200, 75, 75);
+    ellipse(485, 215, 60, 60);
+    ellipse(525, 225, 75, 75);
+    ellipse(565, 215, 60, 60);
+    
 }
