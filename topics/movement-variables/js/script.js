e3a5ca44a5cfb4bb950b245d4a380ceee4321067
Sarah <smroy8@ulaval.ca>
2026-09-18 16:46:49 -0400

    creating the base of the project

--- topics/movement-variables/js/script.js
+++ topics/movement-variables/js/script.js
@@ -1,24 +1,39 @@
 /**
- * Title of Project
- * Author Name
+ * A bird...
+ * Sarah-Maude Roy
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Drawing a bird
+ * 
  */
 
 "use strict";
 
+//creating object bird
+let bird = {
+    x: 120,
+    y: 480,
+    size: 50
+}
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * creating a canvas
 */
 function setup() {
-
+    createCanvas(640, 480);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * drawing a bird
 */
 function draw() {
+    background(0);
+    
+    // Move the bird
+    bird.x = bird.x + 1;
+    bird.y = bird.y - 2;
+    
+    // Draw the bird
+    ellipse(bird.x, bird.y, bird.size);
 
 }
