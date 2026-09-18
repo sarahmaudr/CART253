1b67448073bef289abb8be51829d47dd03f78cb3
Sarah <smroy8@ulaval.ca>
2026-09-18 16:39:31 -0400

    adding constrain to create limits within our variables

--- topics/constrain-that-ego/js/script.js
+++ topics/constrain-that-ego/js/script.js
@@ -12,6 +12,8 @@ let ego = {
     x: 200,
     y: 200,
     size: 1,
+    minSize: 0,
+    maxSize: 400,
     fill: 200
 };
 
@@ -32,8 +34,15 @@ function draw() {
     // Update the ego
     // Fill gets darker, symbolizing the negative energy
     ego.fill = ego.fill - 0.5;
+
+    // Constrain the fill to be within the normal range of colours, 0-255
+    ego.fill = constrain(ego.fill, 0, 255);
+
     // Size gets bigger, symbolizing taking up emotional space
     ego.size = ego.size + 1;
+
+    // Constrain the ego to be within its defined minimum and maximum
+    ego.size = constrain(ego.size, ego.minSize, ego.maxSize);
 
     // Draw the ego
     push();
