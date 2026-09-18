8ad226b38dffededa06e9b51c6b6f3b0a9500a1d
Sarah <smroy8@ulaval.ca>
2026-09-18 16:09:49 -0400

    updating our code by organizing with objects

--- topics/self-esteem/js/script.js
+++ topics/self-esteem/js/script.js
@@ -8,58 +8,50 @@
 "use strict";
 
 // Colour of the sky
-let skyRed = 150;
-let skyGreen = 180;
-let skyBlue = 250;
+let sky = {
+  red: 150,
+  green: 180,
+  blue: 250
+};
 
 // The sun
-
-// Colour
 let sun = {
-    red: 255,
-    green: 255,
-    blue: 0,
-// Position and size
-    x: 500,
-    y: 70,
-    size: 100
+  red: 255,
+  green: 255,
+  blue: 0,
+  x: 500,
+  y: 70,
+  size: 100
 };
 
 // My self-esteem
+let selfEsteem = {
+  shade: 0,
+  x: 320,
+  y: 320,
+  size: 20
+};
 
-// Greyscale shade
-let selfEsteemShade = 0;
-// Position and size
-let selfEsteemX = 320;
-let selfEsteemY = 320;
-let selfEsteemSize = 20;
-
-/**
- * Create the canvas
- */
 function setup() {
-    // Create the canvas
-    createCanvas(640, 320);
+  // Create the canvas
+  createCanvas(640, 320);
 }
 
-/**
- * Displays the sky, sun, and self-esteem
- */
 function draw() {
-    // A nice blue sky
-    background(skyRed, skyGreen, skyBlue);
-
-    // The sun
-    push();
-    fill(sun.red, sun.green, sun.blue);
-    noStroke();
-    ellipse(sun.x, sun.y, sun.size);
-    pop();
-
-    // My self esteem
-    push();
-    fill(selfEsteemShade);
-    noStroke();
-    ellipse(selfEsteemX, selfEsteemY, selfEsteemSize);
-    pop();
+  // A nice blue sky
+  background(sky.red, sky.green, sky.blue);
+  
+  // The sun
+  push();
+  fill(sun.red, sun.green, sun.blue);
+  noStroke();
+  ellipse(sun.x, sun.y, sun.size);
+  pop();
+  
+  // My self esteem
+  push();
+  fill(selfEsteem.shade);
+  noStroke();
+  ellipse(selfEsteem.x, selfEsteem.y, selfEsteem.size);
+  pop();
 }
