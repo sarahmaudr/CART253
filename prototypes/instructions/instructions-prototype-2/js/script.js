732fa19b674ce74571f9224ca688df6c012149de
Sarah <smroy8@ulaval.ca>
2026-09-18 19:05:51 -0400

    adding a lot of triangles to imitate mirror reflections

--- prototypes/instructions/instructions-prototype-2/js/script.js
+++ prototypes/instructions/instructions-prototype-2/js/script.js
@@ -1,24 +1,86 @@
 /**
- * Title of Project
- * Author Name
+ * Mirrors
+ * Sarah-Maude Roy
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * This program draws multiple triangles that change color with the movement of the mouse of the user
+ * (similar to the reflexion of mirrors).
+ * 
+ * Uses:
+ * p5.js
+ * https://p5js.org/
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * creating a 800x800 canvas
 */
 function setup() {
-
+    // creates the canvas
+    createCanvas(400,400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * drawing mirrors
 */
 function draw() {
+    drawMirrors();
+}
 
+function drawMirrors() {
+    background(0);
+    push();
+    noStroke();
+    fill(mouseX, mouseY, 0);
+    triangle(-50, 50, 30, -50, 200, 200);
+    pop();
+
+    push();
+    noStroke();
+    fill(mouseX, mouseY, 255);
+    triangle(-50, 50, 0, 200, 200, 200);
+    pop();
+
+    push();
+    noStroke();
+    fill(255, mouseY, mouseX);
+    triangle(0, 200, 0, 400, 200, 200);
+    pop();
+
+    push();
+    noStroke();
+    fill(mouseX, 255, mouseY);
+    triangle(0, 400, 200, 400, 200, 200);
+    pop();
+
+    push();
+    noStroke();
+    fill(mouseY, 255, mouseX);
+    triangle(200, 400, 400, 400, 200, 200);
+    pop();
+
+    push();
+    noStroke();
+    fill(0, mouseX, mouseX);
+    triangle(400, 400, 400, 200, 200, 200);
+    pop();
+
+    push();
+    noStroke();
+    fill(255, mouseX, mouseY);
+    triangle(400, 200, 400, 0, 200, 200);
+    pop();
+
+    push();
+    noStroke();
+    fill(mouseY, 0, mouseY);
+    triangle(400, 0, 250, 0, 200, 200);
+    pop();
+
+    push();
+    noStroke();
+    fill(0, mouseX, mouseY);
+    triangle(250, 0, 30, 0, 200, 200);
+    pop();
 }
