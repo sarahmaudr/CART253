304958a0ae3adbc17cd4081c4d3f33011ea42a64
Sarah <smroy8@ulaval.ca>
2026-09-18 20:46:56 -0400

    drawing eyes

--- prototypes/instructions/creepyFlower_weird/js/script.js
+++ prototypes/instructions/creepyFlower_weird/js/script.js
@@ -28,19 +28,26 @@ function draw() {
     drawBackground();
     drawStem();
     drawPetals();
+    drawHead();
+    drawEyes();
 }
 
 function drawBackground(){
-    background(0);
+    //draw the background of my masterpiece.
+    colorMode(RGB, 600);
+    background(0, 0, 0);
 }
 
 function drawStem(){
+    // draw stem of the flower
     push();
     fill("green");
     rect(290, 390, 20, 400);
+    pop();
 }
 
 function drawPetals(){
+    // draw petals of the flower
     push();
     noStroke();
     fill("white");
@@ -49,5 +56,30 @@ function drawPetals(){
     circle(240, 355, 150);
     circle(360, 355, 150);
     circle(400, 225, 150);
+    pop();
+}
+
+function drawHead() {
+    // draw middle of the flower
+    push();
+    noStroke();
+    fill("yellow");
+    circle(300, 265, 180);
+    pop();
+}
+
+function drawEyes(){
+    //draw weird eyes
+    push();
+    fill(mouseY, 0, 0);
+    ellipse(275, 250, 25, 50);
+    ellipse(325, 250, 25, 50);
+    pop();
+
+    //draw pupils 
+    push();
+    fill(0, 0, 0);
+    circle(275, 250, 10);
+    circle(325, 250, 10);
     pop();
 }
