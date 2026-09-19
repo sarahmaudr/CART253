0ee17d037f03a4f6651382aa1b4277632adea89c
Sarah <smroy8@ulaval.ca>
2026-09-18 20:24:46 -0400

    creating the base of the flower before it gets weird

--- prototypes/instructions/creepyFlower_weird/js/script.js
+++ prototypes/instructions/creepyFlower_weird/js/script.js
@@ -28,5 +28,26 @@ function draw() {
     drawBackground();
     drawStem();
     drawPetals();
-    
+}
+
+function drawBackground(){
+    background(0);
+}
+
+function drawStem(){
+    push();
+    fill("green");
+    rect(290, 390, 20, 400);
+}
+
+function drawPetals(){
+    push();
+    noStroke();
+    fill("white");
+    circle(300, 150, 150);
+    circle(195, 225, 150);
+    circle(240, 355, 150);
+    circle(360, 355, 150);
+    circle(400, 225, 150);
+    pop();
 }
