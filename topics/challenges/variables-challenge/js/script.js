ee13852cd693ee30bbeb88b8448f5c77cec0392b
Sarah <smroy8@ulaval.ca>
2026-09-22 16:00:38 -0400

    organizing

---
+++ topics/challenges/variables-challenge/js/script.js
@@ -0,0 +1,157 @@
+/**
+ * The Artistic chimney
+ * Athanasia Iliopoulos, Razan Elsaygh, Sarah-Maude Roy
+ * 
+ * Drawing a smoking chimney 
+ */
+
+"use strict";
+
+let brush = {
+  x: 500,
+  y: 335,
+  size: 5,
+};
+
+let brush2 = {
+  x: 500,
+  y: 335,
+  size: 5,
+}
+
+let brush3 = {
+  x: 500,
+  y: 335,
+  size: 5,
+}
+
+let brush4 = {
+  x: 500,
+  y: 335,
+  size: 5,
+}
+let brush5 = {
+  x: 500,
+  y: 335,
+  size: 5,
+}
+
+/**
+ * Creating a 1000x640 canvas and a background
+*/
+function setup() {
+    // canvas 
+    createCanvas(1000, 640);
+    // background
+    background("blue");
+}
+
+/**
+ * drawing a chimney 
+*/
+function draw() {
+    // the base of the chimney
+    push();
+    fill("darkRed");
+    strokeWeight(10);
+    rect(400, 370, 200, 350);
+    pop();
+
+    // the top of the chimney
+    push();
+    fill("grey");
+    strokeWeight(10);
+    rect(375, 350, 250, 50);
+    pop();
+
+    //bricks
+    push();
+    fill("red");
+    strokeWeight(5);
+    rect(425, 425, 75, 25);
+    rect(500, 550, 75, 25);
+    rect(425, 600, 75, 25);
+    rect(425, 500, 75, 25);
+    rect(500, 470, 75, 25);
+    pop();
+
+    // the roof
+    push();
+    fill("brown");
+    strokeWeight(10);
+    rect(0, 500, 400, 150);
+    rect(600, 500, 410, 200);
+    pop();
+
+    // other functions
+    updateBrush();
+    drawBrush();
+}
+
+/**
+ * updating the brush size
+ */
+function updateBrush() {
+  // Update position randomly for brush 1
+  brush.x += random(-3, 3);
+  brush.y += random(-2, -0,1);
+
+  // Update position randomly for brush 2
+  brush2.x += random(-3, 3);
+  brush2.y += random(-2, -0,1);
+
+  // Update position randomly for brush 3
+  brush3.x += random(-3, 3);
+  brush3.y += random(-2, -0,1);
+
+  // Update position randomly for brush 4
+  brush4.x += random(-3, 3);
+  brush4.y += random(-2, -0,1);
+
+// Update position randomly for brush 5
+  brush5.x += random(-3, 3);
+  brush5.y += random(-2, -0,1);
+  
+  // Update brush size randomly
+  brush.size += random(-1, 1.5);
+}  
+
+/**
+ * drawing the smoke different color with 'random' function
+ */
+function drawBrush() {
+  // Display the brush 1 with its fill
+  push();
+  noStroke();
+  fill("grey");
+  ellipse(brush.x, brush.y, brush.size);
+  pop();
+
+  // Display the brush 2 with its fill
+  push();
+  noStroke();
+  fill("black");
+  ellipse(brush2.x, brush2.y, brush.size);
+  pop();
+
+  // Display the brush 3 with its fill
+  push();
+  noStroke();
+  fill("white");
+  ellipse(brush3.x, brush3.y, brush.size);
+  pop();
+
+  // Display the brush 4 with its fill
+  push();
+  noStroke();
+  fill("brown");
+  ellipse(brush4.x, brush4.y, brush.size);
+  pop();
+
+  // Display the brush 5 with its fill
+  push();
+  noStroke();
+  fill("lightBlue");
+  ellipse(brush5.x, brush5.y, brush.size);
+  pop();
+}
