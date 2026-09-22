5e8bbf9457aaac782f81a97d5cf18ad0d13c4ff0
Sarah <smroy8@ulaval.ca>
2026-09-22 15:49:39 -0400

    adding random but controlled smoke and a roof

--- topics/variables/variables-challenge/js/script.js
+++ topics/variables/variables-challenge/js/script.js
@@ -1,12 +1,35 @@
 /**
- * Title of Project
- * Author Name
+ * The Artistic chimney
+ * Athanasia Iliopoulos, Razan Elsaygh, Sarah-Maude Roy
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Drawing a smoking chimney 
  */
 
 "use strict";
+
+let brush = {
+  x: 500,
+  y: 335,
+  size: 5,
+};
+
+let brush2 = {
+  x: 500,
+  y: 335,
+  size: 5,
+}
+
+let brush3 = {
+  x: 500,
+  y: 335,
+  size: 5,
+}
+
+let brush4 = {
+  x: 500,
+  y: 335,
+  size: 5,
+}
 
 /**
  * Creating a 1000x640 canvas and a background
@@ -24,7 +47,7 @@ function setup() {
 function draw() {
     // the base of the chimney
     push();
-    fill("red");
+    fill("darkRed");
     strokeWeight(10);
     rect(400, 370, 200, 350);
     pop();
@@ -35,4 +58,60 @@ function draw() {
     strokeWeight(10);
     rect(375, 350, 250, 50);
     pop();
+
+    push();
+    fill("red");
+    strokeWeight(5);
+    rect(425, 425, 75, 25);
+    rect(500, 550, 75, 25);
+    rect(425, 600, 75, 25);
+    rect(425, 500, 75, 25);
+    rect(500, 470, 75, 25);
+    pop();
+
+    push();
+    fill("brown");
+    strokeWeight(10);
+    rect(0, 500, 400, 150);
+    rect(600, 500, 410, 200);
+    pop();
+
+    updateBrush();
+    drawBrush();
+}
+
+function updateBrush() {
+  // Update position randomly
+  brush.x += random(-3, 3);
+  brush.y += random(-2, -0,1);
+
+  brush2.x += random(-3, 3);
+  brush2.y += random(-2, -0,1);
+
+  brush3.x += random(-3, 3);
+  brush3.y += random(-2, -0,1);
+  
+  // Update brush size randomly
+  brush.size += random(-1, 1.5);
+}  
+
+function drawBrush() {
+  // Display the brush with its fill
+  push();
+  noStroke();
+  fill("grey");
+  ellipse(brush.x, brush.y, brush.size);
+  pop();
+
+  push();
+  noStroke();
+  fill("black");
+  ellipse(brush2.x, brush2.y, brush.size);
+  pop();
+
+  push();
+  noStroke();
+  fill("white");
+  ellipse(brush3.x, brush3.y, brush.size);
+  pop();
 }
