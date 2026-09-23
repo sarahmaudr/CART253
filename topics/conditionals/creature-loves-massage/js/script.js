631c582449808e56f38920b8697ad836572457d2
Sarah <smroy8@ulaval.ca>
2026-09-23 18:14:50 -0400

    adding conditions 'AND', 'NOT', and 'OR' to the file

--- topics/conditionals/creature-loves-massage/js/script.js
+++ topics/conditionals/creature-loves-massage/js/script.js
@@ -23,7 +23,8 @@ const creature = {
     fills: {
         bored: "#000000", // Black
         happy: "#33cc33", // Green
-        angry: "#cc3333" // Red
+        angry: "#cc3333", // Red
+        dead: "#777777" // Grey
     }
 };
 
@@ -48,26 +49,22 @@ function draw() {
  * Responds to user input
  */
 function checkInput() {
-    // Check if the mouse is pressed...
-    if (mouseIsPressed) {
-        // The mouse is pressed!
-        // Change the colour of the creature to show it's happy
-        // It likes the mouse! Squeak squeak!
+    const distance = dist(mouseX, mouseY, creature.x, creature.y);
+    const mouseIsOverlapping = (distance < creature.size/2);
+    const mouseIsMoving = (movedX !== 0 || movedY !== 0);
+   
+    if (mouseIsOverlapping && mouseIsMoving) {
         creature.fill = creature.fills.happy;
     }
-    // Mouse if not pressed, check if a key is pressed...
-    else if (keyIsPressed) {
-        // A key is pressed!
-        // Change the colour of the creature to show it's angry
-        // It hates those keys! Ugh!
-        creature.fill = creature.fills.angry;
-    }
     else {
-        // The mouse isn't pressed and no key is pressed!
-        // Change the colour of the creature to show it's bored
         creature.fill = creature.fills.bored;
     }
 }
+
+function creatureHappy() {
+    creature.fill = creature.fills.happy;
+}
+
 
 /**
  * Draws the creature
