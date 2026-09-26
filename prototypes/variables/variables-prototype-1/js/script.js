0aba4b2a69a06f5afc7974b4a367974a4a400772
Sarah <smroy8@ulaval.ca>
2026-09-26 13:15:04 -0400

    adding final touches and comments

--- prototypes/variables/variables-prototype-1/js/script.js
+++ prototypes/variables/variables-prototype-1/js/script.js
@@ -1,5 +1,5 @@
 /**
- * I AM ON FIRE !
+ * MY HEAD IS ON FIRE !
  * Sarah-Maude Roy
  * 
  * An artwork that shows a face slowly catching on fire.
@@ -11,19 +11,19 @@
 
 "use strict";
 
-
-let fire = {
+// creating the Fire with base settings
+let flame = {
     x: 470,
     y: 300,
     size: 35
 };
-
-let fire2 = {
+// creating the second Fire with base settings
+let flame2 = {
     x: 500,
     y: 300,
     size: 35
 };
-
+// creating the smoke with base settings
 let smoke = {
     x: 555,
     y: 300,
@@ -43,7 +43,7 @@ function setup() {
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * drawing the head catching on fire
 */
 function draw() {
     updateFire();
@@ -51,36 +51,50 @@ function draw() {
     drawHead();
 }
 
+/**
+ * Updates the position of the fire and smoke randomly since it is impredictable
+ */
 function updateFire() {
-    fire.x += random(-4, 4);
-    fire.y += random(-5, 3);
+    // the first big flame
+    flame.x += random(-4, 4);
+    flame.y += random(-5, 3);
 
-    fire2.x += random(-4, 4);
-    fire2.y += random(-5, 3);
+    // the second big flame
+    flame2.x += random(-4, 4);
+    flame2.y += random(-5, 3);
 
+    // the smoke
     smoke.x += random(-4, 4);
     smoke.y += random(-5, 3);
 
-    fire.size += random(-1, 1.5);
-    fire2.size += random(-1, 1.5);
+    // giving random size as the flames and smoke change position
+    flame.size += random(-1, 1.5);
+    flame2.size += random(-1, 1.5);
     smoke.size += random(-1, 1);
 }
 
+/**
+ * Draws the fire with different colored flames and smoke
+ */
 function drawFire() {
+    // first flame is orange 
     push();
     noStroke();
     fill("orange");
-    ellipse(fire.x, fire.y, fire.size);
-    ellipse(fire.x, fire.y, fire.size);
+    // 2 ellipses don't give the same result as 1 for some reason
+    ellipse(flame.x, flame.y, flame.size);
+    ellipse(flame.x, flame.y, flame.size);
     pop();
 
+    // second flame is red
     push();
     noStroke();
     fill("red");
-    ellipse(fire2.x, fire2.y, fire2.size);
-    ellipse(fire2.x, fire2.y, fire2.size);
+    ellipse(flame2.x, flame2.y, flame2.size);
+    ellipse(flame2.x, flame2.y, flame2.size);
     pop();
 
+    // the grey smoke
     push();
     noStroke();
     fill("grey");
@@ -89,13 +103,18 @@ function drawFire() {
     pop();
 }
 
+/**
+ * draws a big head with a scared face
+ */
 function drawHead() {
+    // the big head
     push();
     noStroke();
     fill("yellow");
     circle(500, 400, 300);
     pop();
 
+    // the face
     push();
     fill("black");
     circle(460, 285, 50);
