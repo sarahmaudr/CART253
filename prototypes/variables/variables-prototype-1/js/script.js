f0d25706a7c1b7b83a83223d17b7b5a3661ba0a9
Sarah <smroy8@ulaval.ca>
2026-09-26 12:49:58 -0400

    adding fire and smoke using variables and 'random'

--- prototypes/variables/variables-prototype-1/js/script.js
+++ prototypes/variables/variables-prototype-1/js/script.js
@@ -11,6 +11,25 @@
 
 "use strict";
 
+
+let fire = {
+    x: 470,
+    y: 300,
+    size: 35
+};
+
+let fire2 = {
+    x: 500,
+    y: 300,
+    size: 35
+};
+
+let smoke = {
+    x: 555,
+    y: 300,
+    size: 35
+};
+
 /**
  * create a black canvas
 */
@@ -27,24 +46,60 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-    drawHead();
+    updateFire();
     drawFire();
+    drawHead();
+}
+
+function updateFire() {
+    fire.x += random(-4, 4);
+    fire.y += random(-5, 3);
+
+    fire2.x += random(-4, 4);
+    fire2.y += random(-5, 3);
+
+    smoke.x += random(-4, 4);
+    smoke.y += random(-5, 3);
+
+    fire.size += random(-1, 1.5);
+    fire2.size += random(-1, 1.5);
+    smoke.size += random(-1, 1);
+}
+
+function drawFire() {
+    push();
+    noStroke();
+    fill("orange");
+    ellipse(fire.x, fire.y, fire.size);
+    ellipse(fire.x, fire.y, fire.size);
+    pop();
+
+    push();
+    noStroke();
+    fill("red");
+    ellipse(fire2.x, fire2.y, fire2.size);
+    ellipse(fire2.x, fire2.y, fire2.size);
+    pop();
+
+    push();
+    noStroke();
+    fill("grey");
+    ellipse(smoke.x, smoke.y, smoke.size);
+    ellipse(smoke.x, smoke.y, smoke.size);
+    pop();
 }
 
 function drawHead() {
     push();
+    noStroke();
     fill("yellow");
-    circle(500, 300, 250);
+    circle(500, 400, 300);
     pop();
 
     push();
     fill("black");
-    circle(460, 275, 50);
-    circle(540, 275, 50);
+    circle(460, 285, 50);
+    circle(540, 285, 50);
     ellipse(500, 350, 50, 75);
     pop();
-}
-
-function drawFire() {
-    
 }
