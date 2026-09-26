04afb720f11fbc2e352dc6794f83041b429e8126
Sarah <smroy8@ulaval.ca>
2026-09-26 11:56:54 -0400

    creating the background and canvas to start the project

---
+++ prototypes/variables/variables-prototype-1/js/script.js
@@ -0,0 +1,30 @@
+/**
+ * I AM ON FIRE !
+ * Sarah-Maude Roy
+ * 
+ * An artwork that shows a face slowly catching on fire.
+ * 
+ * Uses:
+ * p5.js
+ * https://p5js.org/
+ */
+
+"use strict";
+
+/**
+ * create a black canvas
+*/
+function setup() {
+    // create a 1000x600 canvas
+    createCanvas(1000, 600);
+    // a black background
+    background("black");
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
