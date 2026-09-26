06b7bc1b242e2872f1055d1803ddd8235c3f18f8
Sarah <smroy8@ulaval.ca>
2026-09-26 14:37:20 -0400

    adding the base functions and some objects

---
+++ prototypes/variables/variables-prototype-3/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    // a 1000x600 canvas
+    createCanvas(1000, 600);
+    // the black background
+    background("lightBlue");
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
