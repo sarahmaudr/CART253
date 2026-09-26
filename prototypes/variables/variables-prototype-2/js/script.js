385ca417fdcdc3948916842eaff66f3fd99d3511
Sarah <smroy8@ulaval.ca>
2026-09-26 13:31:35 -0400

    adding a canvas and background and the base of my new functions

---
+++ prototypes/variables/variables-prototype-2/js/script.js
@@ -0,0 +1,39 @@
+/**
+ * Nightmare
+ * Sarah-Maude Roy
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * 
+ * Uses:
+ * p5.js
+ * https://p5js.org/
+ */
+
+"use strict";
+
+/**
+ * Draws a base canvas with a black background
+*/
+function setup() {
+    // a 1000x600 canvas
+    createCanvas(1000, 600);
+    // the black background
+    background("black");
+}
+
+/**
+ * draws a face you can only see in your nightmares
+*/
+function draw() {
+    drawFace();
+    drawTeeth();
+}
+
+function drawFace() {
+
+}
+
+function drawTeeth() {
+    
+}
